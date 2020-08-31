a0937122613b2ed6393e72d68b27ffb57b64ad65
yfsoftcom <yfsoftcom@126.com>
2020-08-31 17:33:38 +0800

    todo implement the common biz

--- plugins/pg/pg.go
+++ plugins/pg/pg.go
@@ -9,6 +9,15 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type queryReq struct {
+	Table     string `json:"table,omitempty"`
+	Condition string `json:"condition,omitempty"`
+	Skip      int    `json:"skip,omitempty"`
+	Limit     int    `json:"limit,omitempty"`
+	ID        int64  `json:"id,omitempty"`
+	Sort      string `json:"sort,omitempty"`
+}
+
 func init() {
 	fpm.Register(func(app *fpm.Fpm) {
 		option := &plugins.DBSetting{}
@@ -20,5 +29,28 @@ func init() {
 		app.SetDatabase("pg", func() db.Database {
 			return dbclient
 		})
+		bizModule := make(fpm.BizModule, 0)
+
+		// support:
+		// 1. 'find', 'first', 'create', 'update', 'remove', 'clear', 'get', 'count', 'findAndCount'
+
+		bizModule["find"] = func(param *fpm.BizParam) (data interface{}, err error) {
+			// queryReq := queryReq{}
+			// err = param.Convert(&queryReq)
+			// if err != nil {
+			// 	return
+			// }
+
+			// q := db.NewQuery()
+			// q.SetTable(queryReq.Table)
+			// q.SetPager(db.Pagination{
+			// 	Skip:  queryReq.Skip,
+			// 	Limit: queryReq.Limit,
+			// })
+			// err = dbclient.First(q, &one)
+			return
+		}
+
+		app.AddBizModule("common", &bizModule)
 	})
 }
